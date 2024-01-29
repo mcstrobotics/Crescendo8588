292b67926fb05821fbc404bec20c4b59d9ab6aac
MihirMalaviya <mihirmalaviya1@gmail.com>
2024-01-29 12:02:36 -0500

    setting up smartdashboard

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 // import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -36,19 +38,28 @@ public class IntakeSubsystem extends SubsystemBase {
 
   /** move intake motor to suck the note in */
   public void intakeIn() {
-    System.out.println("Intake in");
+    // System.out.println("Intake in");
+    SmartDashboard.putString("Intake State", "In");
+    SmartDashboard.putNumber("Intake Velocity", IntakeConstants.kIntakeInSpeed);
+
     chassis.getIntake().set(IntakeConstants.kIntakeInSpeed);
   }
 
   /** move intake motor to push the note out */
   public void intakeOut() {
-    System.out.println("Intake out");
+    // System.out.println("Intake out");
+    SmartDashboard.putString("Intake State", "Out");
+    SmartDashboard.putNumber("Intake Velocity", -IntakeConstants.kIntakeOutSpeed);
+
     chassis.getIntake().set(-IntakeConstants.kIntakeOutSpeed);
   }
 
   /** stop intake motor */
   public void intakeStop() {
-    System.out.println("Intake stopped");
+    // System.out.println("Intake stopped");
+    SmartDashboard.putString("Intake State", "Stopped");
+    SmartDashboard.putNumber("Intake Velocity", 0);
+
     chassis.getIntake().set(0);
   }
 
